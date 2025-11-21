c23e8065cebb10a8ec2360231d22ed00f5db9f02
alexiokgb22 <varissoualexio@gmail.com>
2025-11-21 17:33:20 +0100

    feat: auth and roles setup
    - install Laravel Breeze
    - Add avatar column to users table
    - Install Spatie Permission package
    - Configure User model with HasRoles
    - Seed database with Admin and Users roles

--- lara-knowlegde/database/seeders/DatabaseSeeder.php
+++ lara-knowlegde/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,12 +15,33 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
+    { // 1. Création des rôles
+        $adminRole = Role::create(['name' => 'admin']);
+        $userRole = Role::create(['name' => 'user']);
+ 
+        // 2. Création de l'administrateur
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@test.com',
+            'password' => bcrypt('password'),
+        ]);
+ 
+        $admin->assignRole($adminRole);
+ 
+        // 3. Création des utilisateurs standards
+        $user = User::create([
+            'name' => 'User',
+            'email' => 'user@test.com',
+            'password' => bcrypt('password'),
+        ]);
+ 
+        $user->assignRole($userRole); // Attribution du rôle 'user'
+ 
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '49a1e4407f2f8339e4860f971a9f7c498c5864f7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '49a1e4407f2f8339e4860f971a9f7c498c5864f7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
